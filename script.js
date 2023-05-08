b421eef1ae111ed38f5bd90b74b1f090e39a664f
WhereIsLijah <ayomideoduba@gmail.com>
2023-05-08 01:56:27 -0400

    Display Value error fix

--- script.js
+++ script.js
@@ -1,58 +1,32 @@
-function populate(buttonId){
-
-    switch (buttonId) {
-                case "myNum0":
-                    return 0;
-                case "myNum1":
-                    return 1;
-                case '-':
-                    return 2;
-                case '*':
-                    return 3;
-                case '/':
-                    return divideNum(num1, num2);
-    }
-
-}
-
-// function buttonEvent(buttonId) {
-//     const button = buttonId.value;
-//     console.log("button value:", button)
-//     const display = document.getElementById("res_display");
-    
-//     display.textContent += button;
-
-// }
+let displayValue = '';
+let optr;
 
 
 function buttonEvent(buttonId) {
-    const button = document.getElementById(buttonId);
-    console.log("Button id:",buttonId)
-    const buttonValue = button.textContent;
-    console.log("button Value:", buttonValue)
+    const button = document.getElementById(buttonId);  
     const display = document.getElementById("res_display");
   
-    display.textContent += buttonValue;
+    display.textContent += button.textContent;
+    displayValue += display.textContent;
+  }
+
+function operator(optr) {
+    const button = document.getElementById(optr);  
+    const display = document.getElementById("res_display");
+  
+    display.textContent = button.textContent;
+    optr = display.textContent;
+
   }
 
   
 
-// function buttonEvent(buttonId) {
-//     const button = document.getElementById(buttonId);
-//     const display = document.getElementById("res_display");
-  
-//     button.onclick = function() {
-//       const number = button.textContent;
-//       console.log(number);
-//       if (display.textContent === "") {
-//         display.textContent = number;
-//         console.log("empty string")
-//       } else {
-//         display.textContent += number;
-//         console.log("No empty string")
-//       }
-//     };
-//   }
+  button.addEventListener("click", function () {
+    display.textContent = ;
+
+
+    display.textContent += button.textContent;
+});
   
 
 const addNum = (num1, num2) => num1 + num2;
@@ -62,17 +36,8 @@ const divideNum = (num1, num2) => num1 / num2;
 const modNum = (num1, num2) => num1 % num2;
 const plusMinus = (num) => num * - 1;
 
-function operate(optr, num1, num2) {
-    switch (optr) {
-        case '+':
-            return addNum(num1, num2);
-        case '-':
-            return subNum(num1, num2);
-        case '*':
-            return multiplyNum(num1, num2);
-        case '/':
-            return divideNum(num1, num2);
-    }
+function operate(optr) {
+
 }
 
 
