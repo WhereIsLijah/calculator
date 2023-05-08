05d99d684fd050d4343c38157d2a6abd34664d69
WhereIsLijah <ayomideoduba@gmail.com>
2023-05-07 23:46:48 -0400

    Restructuring Calculation Functions

--- script.js
+++ script.js
@@ -15,16 +15,45 @@ function populate(buttonId){
 
 }
 
+// function buttonEvent(buttonId) {
+//     const button = buttonId.value;
+//     console.log("button value:", button)
+//     const display = document.getElementById("res_display");
+    
+//     display.textContent += button;
+
+// }
+
+
 function buttonEvent(buttonId) {
     const button = document.getElementById(buttonId);
+    console.log("Button id:",buttonId)
+    const buttonValue = button.textContent;
+    console.log("button Value:", buttonValue)
     const display = document.getElementById("res_display");
+  
+    display.textContent += buttonValue;
+  }
 
-    button.addEventListener("click", function () {
- 
+  
 
-        display.textContent += button.textContent;
-    });
-}
+// function buttonEvent(buttonId) {
+//     const button = document.getElementById(buttonId);
+//     const display = document.getElementById("res_display");
+  
+//     button.onclick = function() {
+//       const number = button.textContent;
+//       console.log(number);
+//       if (display.textContent === "") {
+//         display.textContent = number;
+//         console.log("empty string")
+//       } else {
+//         display.textContent += number;
+//         console.log("No empty string")
+//       }
+//     };
+//   }
+  
 
 const addNum = (num1, num2) => num1 + num2;
 const subNum = (num1, num2) => num1 - num2;
@@ -33,8 +62,8 @@ const divideNum = (num1, num2) => num1 / num2;
 const modNum = (num1, num2) => num1 % num2;
 const plusMinus = (num) => num * - 1;
 
-function operate(operator, num1, num2) {
-    switch (operator) {
+function operate(optr, num1, num2) {
+    switch (optr) {
         case '+':
             return addNum(num1, num2);
         case '-':
@@ -48,10 +77,12 @@ function operate(operator, num1, num2) {
 
 
 
-//Change background colors of button
-//Design Iphone Shape, add clock and battery level
-//Use DOM for viewing input text
-//create hover for buttons
-//test functions
-//use switch case + DOM for number selection
-//add copyright with github link
+// Change background colors of button
+// Design Iphone Shape, add clock and battery level
+// Use DOM for viewing input text
+// create hover for buttons
+// test functions
+// use switch case + DOM for number selection
+// add copyright with github link
+
+
